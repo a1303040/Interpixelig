8c1e7a0c4d3ee1b4f8f4c4cbf2c237d253160bc9
emanuelstadler <emanuel.stadler@gmail.com>
2015-05-14 17:54:51 +0200

    add rudimentary audioplayer

--- java/itm/audio/AudioPlayer.java
+++ java/itm/audio/AudioPlayer.java
@@ -8,12 +8,7 @@ package itm.audio;
 import java.io.File;
 import java.io.IOException;
 
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.SourceDataLine;
-import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.sound.sampled.*;
 
 /**
  * Plays an audio file using the system's default sound output device
@@ -90,8 +85,35 @@ public class AudioPlayer {
 		// get decoded format
 		
 		// get decoded audio input stream
- 
-		return din;
+
+
+        // from http://archive.oreilly.com/onjava/excerpt/jenut3_ch17/examples/PlaySoundStream.java
+        din=AudioSystem.getAudioInputStream(input);
+        AudioFormat format = din.getFormat( );
+        DataLine.Info info=new DataLine.Info(SourceDataLine.class,format);
+
+        // If the format is not supported directly (i.e. if it is not PCM
+        // encoded), then try to transcode it to PCM.
+        if (!AudioSystem.isLineSupported(info)) {
+            // This is the PCM format we want to transcode to.
+            // The parameters here are audio format details that you
+            // shouldn't need to understand for casual use.
+            AudioFormat pcm =
+                    new AudioFormat(format.getSampleRate( ), 16,
+                            format.getChannels( ), true, false);
+
+            // Get a wrapper stream around the input stream that does the
+            // transcoding for us.
+            din = AudioSystem.getAudioInputStream(pcm, din);
+
+            // Update the format and info variables for the transcoded data
+            format = din.getFormat( );
+            info = new DataLine.Info(SourceDataLine.class, format);
+        }
+
+
+
+        return din;
 	}
 
 	/**
@@ -120,7 +142,49 @@ public class AudioPlayer {
 		// read samples from audio and write them to the data line 
 
 		// properly close the line!
-	}
+
+
+        // Open the line through which we'll play the streaming audio.
+        AudioFormat format = audio.getFormat( );
+        DataLine.Info info=new DataLine.Info(SourceDataLine.class,format);
+
+        SourceDataLine line = (SourceDataLine) AudioSystem.getLine(info);
+        line.open(format);
+
+        line.start();
+
+        //from http://jan.newmarch.name/LinuxSound/Sampled/JavaSound/
+
+        // Create a buffer for moving data from the audio stream to the line.
+        int bufferSize = (int) format.getSampleRate() * format.getFrameSize();
+        byte [] buffer = new byte[ bufferSize ];
+
+        // Move the data until done or there is an error.
+        try {
+            int bytesRead = 0;
+            while (bytesRead >= 0) {
+                bytesRead = audio.read(buffer, 0, buffer.length);
+                if (bytesRead >= 0) {
+                    // System.out.println("Play.playAudioStream bytes read=" + bytesRead +
+                    // ", frame size=" + audioFormat.getFrameSize() + ", frames read=" + bytesRead / audioFormat.getFrameSize());
+                    // Odd sized sounds throw an exception if we don't write the same amount.
+                    int framesWritten = line.write(buffer, 0, bytesRead);
+                }
+            } // while
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Play.playAudioStream draining line.");
+        // Continues data line I/O until its buffer is drained.
+        line.drain();
+
+        System.out.println("Play.playAudioStream closing line.");
+        // Closes the data line, freeing any resources such as the audio device.
+        line.close();
+
+
+    }
 
 	/**
 	 * Main method. Parses the commandline parameters and prints usage
