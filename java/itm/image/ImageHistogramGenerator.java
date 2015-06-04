667e78afe20797b77e3d482343ae519a5f9ef0ed
emanuelstadler <emanuel.stadler@gmail.com>
2015-06-05 00:16:20 +0200

    histogram size change

--- java/itm/image/ImageHistogramGenerator.java
+++ java/itm/image/ImageHistogramGenerator.java
@@ -136,7 +136,7 @@ public class ImageHistogramGenerator {
         histogram.setHistogram(histArray);
 
         // plot the histogram, try different dimensions for better visualization
-        BufferedImage plottedHistogram = histogram.plotHistogram(200, 200);
+        BufferedImage plottedHistogram = histogram.plotHistogram(200, 150);
 
         // encode and save the image as png
         ImageIO.write(plottedHistogram, "png", outputFile);
